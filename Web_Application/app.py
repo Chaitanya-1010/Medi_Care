3ebf319506826efbd9570f6506d4fa804be9ad96
Chaitanya Chitta <srichaitanya1010@gmail.com>
2024-03-25 17:13:58 +0530

    new build

---
+++ Web_Application/app.py
@@ -0,0 +1,74 @@
+from flask import request, jsonify, Flask, send_file
+# from app_routes import app
+# from app.patient_operations import * 
+import Backend.patient_operations as patient
+import Backend.doctors_operations as doctor
+# from app.doctors_operations import * 
+from Backend.login import *
+from flask_cors import CORS
+
+# Define API endpoints
+# app=Flask(__name__)
+app = Flask(__name__, static_folder='static')
+CORS(app) 
+
+@app.route('/')
+def index():
+    # Implement login logic here
+    return send_file(r'Backend\static\dist\Medico\index.html')
+
+# Login
+@app.route('/api/user_login', methods=['POST'])
+def userLogin():
+    # Implement login logic here
+    login_user=request.json
+    return patient.user_login(login_user)
+    pass
+
+@app.route('/api/doctor_login', methods=['POST'])
+def doctorLogin():
+    login_doctor = request.json
+    return doctor.doctor_login(login_doctor)
+
+@app.route('/api/doctor_signup', methods=['POST'])
+def doctor_signup():
+    new_doctor= request.json
+    return doctor.create_doctor(new_doctor)
+    # pass
+
+@app.route('/api/patient_signup', methods=['POST'])
+def patient_signup():
+    new_user= request.json
+    return patient.create_user(new_user)
+
+@app.route('/api/get_doctors', methods=['GET'])
+def getDoctors():
+    return patient.get_doctors()
+    # pass
+
+
+# Patient Operations
+@app.route('/api/user_medical-records', methods=['GET'])
+def get_medical_records():
+    # Implement logic to fetch medical records from MongoDB
+    pass
+
+@app.route('/api/book-appointment', methods=['POST'])
+def book_appointment():
+    # Implement logic to book appointment
+    pass
+
+# Doctor Operations
+@app.route('/api/doctor_appointments', methods=['GET'])
+def get_appointments():
+    # Implement logic to fetch appointments for the doctor
+    pass
+
+@app.route('/api/doctor_respond-appointment', methods=['POST'])
+def respond_appointment():
+    # Implement logic for doctor to respond to appointment requests
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
